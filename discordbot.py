c62468f90acbe5b5fca567b53e8dfc0f6abea0be
Sky167com <84136657+Sky167com@users.noreply.github.com>
2021-05-17 22:28:43 +0900

    ４

--- discordbot.py
+++ discordbot.py
@@ -22,5 +22,25 @@ async def ping(ctx):
 async def hi(ctx):
     await ctx.send('hi')
 
-bot.run(token)
-                  
+
+date = datetime.datetime.now()
+hour = date.hour
+min = date.minute
+
+@client.event
+async def on_ready():
+    print('時報機が起動しました。')
+
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    if message.content == '何時？':
+        await message.channel.send(str(hour) + '時です。')
+    if message.content == '何分？':
+        await message.channel.send(str(min) + '分です。')
+    if message.content == '何時何分？':
+        await message.channel.send(str(hour) + '時' + str(min) + '分です。')
+
+
+bot.run(token)                  
