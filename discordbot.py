426197fc7f437d3c62966205f706fa65131a19a9
Sky167com <84136657+Sky167com@users.noreply.github.com>
2021-05-19 13:08:35 +0900

    3

--- discordbot.py
+++ discordbot.py
@@ -6,33 +6,12 @@ import datetime
 now = datetime.datetime.now()
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
-date = datetime.datetime.now()
-hour = date.hour
-min = date.minute
-
 
 @bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
-
-
-@bot.event
-async def on_ready():
-    print('時報機が起動しました。')
-
-
-@bot.event
-async def on_message(message):
-    if message.author.bot:
-        return
-    if message.content == '何時？':
-        await message.channel.send(str(hour) + '時です。')
-    if message.content == '何分？':
-        await message.channel.send(str(min) + '分です。')
-    if message.content == '何時何分？':
-        await message.channel.send(str(hour) + '時' + str(min) + '分です。')
 
 
 @bot.command()        
@@ -47,7 +26,8 @@ async def hi(ctx,name):
     
 @bot.command()
 async def time(ctx):
-    await ctx.send(now)
+    await import (now)
+    ctx.send(now)
     
     
 bot.run(token)                  
