ab10af4f36901ad1e40120f04cb42186d41308de
Sky167com <84136657+Sky167com@users.noreply.github.com>
2021-06-22 12:59:33 +0900

    3

--- discordbot.py
+++ discordbot.py
@@ -35,5 +35,12 @@ async def hi(ctx):
 async def 時間(ctx):
     await ctx.send('今は'+str(datetime.datetime.now(tokyo_tz))+'です。')
 #ヒントを作る    
-    
+@bot.command()
+# 返信する非同期関数を定義
+async def reply(message):
+    reply = f'{message.author.mention} 呼んだ？' # 返信メッセージの作成
+    await message.channel.send(reply) # 返信メッセージを送信
+
+
+
 bot.run(token)                  
