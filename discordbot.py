155621ebb61cf98e310bbca83b9dda769edd862f
Sky167com <84136657+Sky167com@users.noreply.github.com>
2021-06-23 14:52:55 +0900

    3

--- discordbot.py
+++ discordbot.py
@@ -14,13 +14,15 @@ async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
-@bot.event# 返信する非同期関数を定義
+
+    
+@bot.command()# 返信する非同期関数を定義
 async def reply(message):
     reply = f'{message.author.mention} 呼んだ？' # 返信メッセージの作成
     await message.channel.send(reply) # 返信メッセージを送信
 
 # 発言時に実行されるイベントハンドラを定義
-@bot.event
+@bot.command()
 async def on_message(message):
     if client.user in message.mentions: # 話しかけられたかの判定
         await reply(message) # 返信する非同期関数を実行
